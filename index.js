c83e8ef6f95f65f24b4d23daaad6b4f029a74eb8
Sam Fullerton <Samdfullerton@gmail.com>
2020-10-15 10:07:32 -0500

    added prompt

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
+const fs = require("fs")
+const inquirer = require("inquirer")
+const generateMarkDown = require("./utils/generateMarkdown")
+
 // array of questions for user
 const questions = [
-    
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Describe your project"
+    }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+   return fs.writeFileSync(fileName, data)
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions).then(function(responses){
+        console.log(responses)
+        writeToFile("README.md", generateMarkDown(responses))
+    })
 }
 
 // function call to initialize program
